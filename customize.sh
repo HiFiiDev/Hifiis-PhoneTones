0937b298a8d994218d8e4bb4d58be25472fc3267
HifiiDev <hifii@outlook.com>
2022-04-14 00:14:32 -0700

    updates

--- customize.sh
+++ customize.sh
@@ -1,69 +1,107 @@
 ##########################################################################################
 #
-# MMT Extended Config Script
-#
+# Magisk Module Template Config Script
+# by topjohnwu
+# 
+##########################################################################################
+##########################################################################################
+# 
+# Instructions:
+# 
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (common/config.sh)
+# 4. For advanced features, add shell commands into the script files under common:
+#    post-fs-data.sh, service.sh
+# 5. For changing props, add your additional/modified props into common/system.prop
+# 
 ##########################################################################################
 
 ##########################################################################################
-# Config Flags
+# Defines
 ##########################################################################################
 
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-#MINAPI=21
-#MAXAPI=25
-#DYNLIB=true
-#DEBUG=true
+# NOTE: This part has to be adjusted to fit your own needs
+
+# This will be the folder name under /magisk
+# This should also be the same as the id in your module.prop to prevent confusion
+MODID=phonetones
+
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=true
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=false
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print "**********************"
+  ui_print " Hifiis Phone Tones!! "
+  ui_print "**********************"
+}
 
 ##########################################################################################
 # Replace list
 ##########################################################################################
 
 # List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
+# By default Magisk will merge your files with the original system
+# Directories listed here however, will be directly mounted to the correspond directory in the system
 
-# Construct your list in the following format
+# You don't need to remove the example below, these values will be overwritten by your own list
 # This is an example
-REPLACE_EXAMPLE="
+REPLACE="
 /system/app/Youtube
 /system/priv-app/SystemUI
 /system/priv-app/Settings
 /system/framework
 "
 
-# Construct your own list here
+# Construct your own list here, it will overwrite the example
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 /system/product/media/audio/
 /system/product/media/audio/ringtones
+/system/media
+/system/media/audio
+/system/product/media/audio/notifications/Popcorn.ogg
 "
 
 ##########################################################################################
 # Permissions
 ##########################################################################################
 
+# NOTE: This part has to be adjusted to fit your own needs
+
 set_permissions() {
-  : # Remove this if adding to this function
-
-  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
-  # Some examples:
+  # Default permissions, don't remove them
+  set_perm_recursive  $MODPATH/system/media/  0  0  0755  0644
   
-  # For directories (includes files in them):
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Some templates if you have no idea what to do:
+
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  
-  set_perm_recursive $MODPATH/system/product/media 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
 
-  # For files (not in directories taken care of above)
   # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
 }
-
-##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
-##########################################################################################
 
 SKIPUNZIP=1
 unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
